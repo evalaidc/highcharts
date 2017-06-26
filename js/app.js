2523d3df23023abe7037cf22976d937ab3e1fda6
Eva Lai <evalai.dc@gmail.com>
2017-06-25 22:26:48 -0400

    added router and home page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 angular
   .module("Highcharts", [
-
+    "ui.router",
+    "ngResource"
   ])
-  .config([])
+  .config([
+    "$stateProvider",
+    "$urlRouterProvider",
+    RouterFunction
+  ])
+  .factory("DataFactory", [
+    "$resource",
+    DataFactoryFunction
+  ])
+  .controller("dataIndexCtrl", [
+    "$state",
+    "DataFactory",
+    dataIndexControllerFunction
+  ])
+
+  function RouterFunction($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("Home", {
+        url: "/",
+        templateUrl: "js/ng-views/home.html"
+      })
+  }
