8825e6875e8fd14644940e757014dee2914aa421
Eva Lai <evalai.dc@gmail.com>
2017-06-25 23:22:30 -0400

    added express & attempted to start app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.set("port", process.env.PORT || 3001)
+
+
+
+app.listen(app.get("port"), () => {
+  console.log('App is listening')
+})
--- js/app.js
+++ js/app.js
@@ -12,23 +12,32 @@ angular
     "$resource",
     DataFactoryFunction
   ])
-  .controller("dataIndexCtrl", [
+  .controller("DataIndexCtrl", [
     "$state",
     "DataFactory",
-    dataIndexControllerFunction
+    DataIndexControllerFunction
   ])
 
   function RouterFunction($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state("Home", {
+      .state("home", {
         url: "/",
         templateUrl: "js/ng-views/home.html"
       })
       .state("dataIndex", {
         url: "/data",
         templateUrl: "js/ng-views/data/index.html",
-        controller: "dataIndexCtrl",
+        controller: "DataIndexCtrl",
         controllerAs: "vm"
       })
       $urlRouterProvider.otherwise("data")
+  }
+
+  function DataFactoryFunction($resource) {
+    return $resource("api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=state:06&LAN=625", {}, {
+    })
+  }
+
+  function DataIndexControllerFunction($state, DataFactory) {
+    this.data = DataFactory.query();
   }
